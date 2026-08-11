59ab343cfec2f6c0a85551c7ef54db0a5f896d45
Abhishek kumar <itsabhipro@gmail.com>
2026-08-11 13:28:50 +0300

    Add utils

---
+++ lib/utils.ts
@@ -0,0 +1,18 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatNumber(n: number, decimals = 1) {
+  return n.toLocaleString("en-US", {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  });
+}
+
+export function formatTons(n: number) {
+  if (n >= 1000) return `${formatNumber(n / 1000)}k`;
+  return formatNumber(n);
+}
